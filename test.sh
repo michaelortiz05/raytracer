e17e94e6cabd252cca6f4ddf937cefa740d3f75a
eric141822 <eric141822@gmail.com>
2023-12-10 21:56:40 -0600

    test.sh

---
+++ test.sh
@@ -0,0 +1,29 @@
+./program ray-1.txt
+
+wait
+
+./program ray-150.txt
+
+wait
+
+./program ray-300.txt
+
+wait
+
+./program ray-625.txt
+
+wait
+
+./program ray-1250.txt
+
+wait
+
+./program ray-2500.txt
+
+wait
+
+./program ray-5000.txt
+
+wait
+
+./program tenthousand.txt
